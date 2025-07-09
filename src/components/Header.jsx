c914aa60695832b980b038ebccbf52617a70dd6c
harisarif <harif3496@gmail.com>
2025-07-09 16:59:09 +0500

    logo added active color fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,105 +1,91 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import logoImg from "../assets/images/logo.svg";
-
-import {SimpleButton, SimpleLink, GradientButton, GradientLink} from '../Partials/CustomButton';
+import { SimpleButton, SimpleLink, GradientButton, GradientLink } from "../Partials/CustomButton";
 
 const Header = () => {
+  const location = useLocation(); // 👈 Get current route path
+
   return (
-    <div>
-      <header>
-        <div className="header-container">
-          <nav className="navbar navbar-expand-md p-0">
-            <div className="container-fluid" style={{maxWidth: 840}}>
-              <div className="img-wrapper">
-                <img src={logoImg} alt="Logo" />
-              </div>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon" />
-              </button>
-
-              <div
-                className="collapse navbar-collapse header-navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/">
-                      AI Gateway
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/Features">
-                      Features
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/pricing">
-                      Pricing
-                    </NavLink>
-                  </li>
-
-                  {/* Dropdown: Resources */}
-                  <li className="nav-item dropdown">
-                    <a
-                      className="nav-link dropdown-toggle"
-                      href="#"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      Resources
-                    </a>
-                    <ul className="dropdown-menu">
-                      <li>
-                        <NavLink
-                          className="dropdown-item"
-                          to="/Blogs"
-                        >
-                          Blogs
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink
-                          className="dropdown-item"
-                          to="/Observability"
-                        >
-                          Observability
-                        </NavLink>
-                      </li>
-                      <li>
-                        <hr className="dropdown-divider" />
-                      </li>
-                      <li>
-                        <NavLink
-                          className="dropdown-item"
-                          to="/resources/something"
-                        >
-                          Something else
-                        </NavLink>
-                      </li>
-                    </ul>
-                  </li>
-
-                </ul>
-                <form className="d-flex" role="search">
-                  <GradientLink text={'Start for free'} to="/login" />
-                </form>
-                {/* Login Button */}
-              </div>
+    <header>
+      <div className="header-container">
+        <nav className="navbar navbar-expand-md p-0">
+          <div className="container-fluid" style={{ maxWidth: 840 }}>
+            <div className="img-wrapper">
+              <img src={logoImg} alt="Logo" />
             </div>
-          </nav>
-        </div>
-      </header>
-    </div>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+
+            <div
+              className="collapse navbar-collapse header-navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
+                  <NavLink className="nav-link" to="/">
+                    AI Gateway
+                  </NavLink>
+                </li>
+                <li className={`nav-item ${location.pathname === '/Features' ? 'active' : ''}`}>
+                  <NavLink className="nav-link" to="/Features">
+                    Features
+                  </NavLink>
+                </li>
+                <li className={`nav-item ${location.pathname === '/pricing' ? 'active' : ''}`}>
+                  <NavLink className="nav-link" to="/pricing">
+                    Pricing
+                  </NavLink>
+                </li>
+
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Resources
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li className={`${location.pathname === '/Blogs' ? 'active' : ''}`}>
+                      <NavLink className="dropdown-item" to="/Blogs">
+                        Blogs
+                      </NavLink>
+                    </li>
+                    <li className={`${location.pathname === '/Observability' ? 'active' : ''}`}>
+                      <NavLink className="dropdown-item" to="/Observability">
+                        Observability
+                      </NavLink>
+                    </li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li className={`${location.pathname === '/resources/something' ? 'active' : ''}`}>
+                      <NavLink className="dropdown-item" to="/resources/something">
+                        Something else
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+
+              <form className="d-flex" role="search">
+                <GradientLink text={'Start for free'} to="/login" />
+              </form>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
   );
 };
 
