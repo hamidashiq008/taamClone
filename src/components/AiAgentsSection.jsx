8aa033983068822c513bd3c9f74ffce139f59b1c
Mubashar Ahmad <MubasharDev0203@gmail.com>
2025-07-10 18:50:56 +0500

    Update

--- src/components/AiAgentsSection.jsx
+++ src/components/AiAgentsSection.jsx
@@ -26,8 +26,8 @@ const AiAgentsSection = () => {
 
         <div className="row justify-content-center g-4">
           {/* Left Card */}
-          <div className="col-md-6">
-            <div className="custom-card h-100">
+          <div className="col-lg-6">
+            <div className="solution-card h-100">
               <div className="custom-card-body">
                 <p className=" mb-2 heading-para">Ready Agentic AI Solutions</p>
                 <p className="small mb-4 description-para">
@@ -46,111 +46,196 @@ const AiAgentsSection = () => {
                 <div className="chat-box ">
                   <div className="chat-box-bg">
                     <div className="bottom-gradient"></div>
+                    <div className="bottom-dot-bg">
+                      <svg width="100%" height="100%" class="text-muted-strong-background inset-0"><defs><pattern id="«R1bacj3kmdb»" width="22" height="22" patternUnits="userSpaceOnUse"><rect x="5.5" y="5.5" width="1" height="1" fill="currentColor"></rect></pattern></defs><rect width="100%" height="100%" fill="url(#«R1bacj3kmdb»)"></rect></svg>
+                    </div>
+                  </div>
+                  <div className="bg-hover-shadow"></div>
+                  <div className="inner-chat-box">
+                    <div className="d-flex flex-column gap-0 h-100">
+                      <div className="p-3 flex-grow-1 content-div">
+
+                        <div className="content-inner-heading">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 20 20">
+                            <path d="M 18.75 12.485 C 14.41 12.485 12.5 14.395 12.5 18.735 C 12.5 14.395 10.59 12.485 6.25 12.485 C 10.59 12.485 12.5 10.575 12.5 6.235 C 12.5 10.575 14.41 12.485 18.75 12.485 Z" fill="transparent" stroke="rgb(255,150,75)" stroke-linejoin="round" stroke-dasharray=""></path>
+                            <path d="M 9.583 5.402 C 6.69 5.402 5.417 6.675 5.417 9.568 C 5.417 6.675 4.144 5.402 1.25 5.402 C 4.144 5.402 5.417 4.128 5.417 1.235 C 5.417 4.128 6.69 5.402 9.583 5.402 Z" fill="transparent" stroke="rgb(255,150,75)" stroke-linejoin="round" stroke-dasharray=""></path>
+                          </svg>
+                          <h2 className="heading">What’s the best model for summarizing legal documents?</h2>
+                        </div>
+
+                        <div className="main-content">
+                          <Typewriter
+                            options={{
+                              strings: [`The AI explains, suggests models, and can even refine responses based on feedback.`],
+                              autoStart: true,
+                              loop: true,
+                              delay: 20,
+                              deleteSpeed: 0,
+                            }}
+                          />
+                        </div>
+
+                        <div className="buttons-div">
+                          <button>Add details</button>
+                          <button>Suggest something else</button>
+                        </div>
+                      </div>
+                      <div className="p-3 border-light-2">
+                        <div className="d-flex gap-3 justify-content-between">
+                          <div className="moveable-content flex-grow-1">
+                            <span>Ask another question</span>
+                          </div>
+                          <div className="icon-div flex-shrink-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-send-2"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4.698 4.034l16.302 7.966l-16.302 7.966a.503 .503 0 0 1 -.546 -.124a.555 .555 0 0 1 -.12 -.568l2.468 -7.274l-2.468 -7.274a.555 .555 0 0 1 .12 -.568a.503 .503 0 0 1 .546 -.124z" /><path d="M6.5 12h14.5" /></svg>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                
+
               </div>
             </div>
           </div>
 
-          {/* Right Card */}
-          <div className="col-md-6">
-            <div className="custom-card h-100">
-              <p className="mb-2 heading-para">
-                AI Search with Verified References
-              </p>
-              <p className="small mb-4 description-para">
-                Deliver answers backed by real-time citations from trusted
-                sources. Our AI search engine not only finds relevant results...
-              </p>
-              <div className="second-icon-circle icon-circle mb-3  ">
-                <i className="bi bi-search text-success"></i>
-              </div>
-
-              <div className="chat-box chat-box-second">
-                <p className="  mb-2 heading">
-                  Built-in the{" "}
-                  <span className="text-success fw-semibold">
-                    smart with Taam
-                  </span>{" "}
-                  AI Search.
+          <div className="col-lg-6">
+            <div className="reference-card h-100">
+              <div className="custom-card-body">
+                <p className=" mb-2 heading-para">AI Search with Verified References</p>
+                <p className="small mb-4 description-para">
+                  Deliver answers backed by real-time citations from trusted sources. Our AI search engine not only finds relevant results
                 </p>
+                <div className="first-icon-circle icon-circle mb-2">
+                  <div className="inner-icon-div">
+                    <div className="border-div">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" focusable="false" color="rgb(127, 247, 189)" style={{ userSelect: 'none', width: '100%', height: '100%', display: 'inline-block', fill: 'rgb(127, 247, 189)', color: 'rgb(127, 247, 189)', flexShrink: 0 }}><g color="rgb(127, 247, 189)" weight="regular"><path d="M227.32,73.37,182.63,28.69a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H216a8,8,0,0,0,0-16H115.32l112-112A16,16,0,0,0,227.32,73.37ZM92.69,208H48V163.31l88-88L180.69,120ZM192,108.69,147.32,64l24-24L216,84.69Z"></path></g></svg>
+                    </div>
+                  </div>
+                  <div class="icon-bottom-line" ></div>
+                </div>
 
-                <div className="references-box mt-3">
-                  <strong className="text-info">📚 References</strong>
-                  <ul className=" small mt-2 mb-3">
-                    <li>Taam AI Blog</li>
-                    <li>John Umbridge</li>
-                    <li>c. 2022</li>
-                  </ul>
-                  <hr className="my-3" />
-                  <div className="d-flex gap-2 btns-wrapper">
-                    <button className="btn btn-outline-light btn-sm">
-                      Most cited
-                    </button>
-                    <button className="btn btn-outline-light btn-sm">
-                      Latest
-                    </button>
+                <div className="pre-chat-box">
+                  Built-in  the <span className="text-green">smart with</span> Taam AI Search
+                </div>
+
+                <div className="chat-box ">
+                  <div className="chat-box-bg">
+                    <div className="bottom-gradient"></div>
+                    <div className="bottom-dot-bg">
+                      <svg width="100%" height="100%" class="text-muted-strong-background inset-0"><defs><pattern id="«R1bacj3kmdb»" width="22" height="22" patternUnits="userSpaceOnUse"><rect x="5.5" y="5.5" width="1" height="1" fill="currentColor"></rect></pattern></defs><rect width="100%" height="100%" fill="url(#«R1bacj3kmdb»)"></rect></svg>
+                    </div>
+                  </div>
+                  <div className="bg-hover-shadow"></div>
+                  <div className="inner-chat-box">
+                    <div className="d-flex flex-column gap-0 h-100">
+                      <div className="p-3 flex-grow-1 content-div">
+
+                        <div className="content-inner-heading">
+                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 18 18">
+                            <path d="M 11.499 6.917 L 7.749 11.5 L 6.082 9.833 M 16.915 9 C 16.915 13.372 13.371 16.917 8.999 16.917 C 4.626 16.917 1.082 13.372 1.082 9 C 1.082 4.628 4.626 1.083 8.999 1.083 C 13.371 1.083 16.915 4.628 16.915 9 Z" fill="transparent" stroke="rgb(127,247,189)" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray=""></path>
+                          </svg>
+                          <h2 className="heading">References</h2>
+                        </div>
+
+                        <div className="content-inner-heading flex-column align-items-start w-100">
+                          <h2 className="heading">LLM transformer models</h2>
+                          <ul className="content-list mb-0">
+                            <li>Taam AI Blog</li>
+                            <li>John Umbridge</li>
+                            <li>c. 2022</li>
+                          </ul>
+                        </div>
+
+
+                      </div>
+                      <div className="p-3 border-light-2">
+                        <div className="buttons-div">
+                          <button>Most cited</button>
+                          <button>Latest</button>
+                        </div>
+                      </div>
+
+                    </div>
                   </div>
                 </div>
+
               </div>
             </div>
           </div>
-          <div className="col-md-7">
-            <div className="custom-card h-100">
-              <p className=" mb-2 heading-para">Intelligent time management</p>
-              <p className="small mb-4 description-para">
-                Manage your AI usage like a pro. Visualize time spent across
-                agents, endpoints, and models—so you can optimize latency,
-                throughput, and productivity.
-              </p>
-              <div className="third-icon-circle icon-circle mb-3">
-                <i className="bi bi-lightning-charge-fill third-icon"></i>
-              </div>
 
-              <div className="chat-box">
-                <p className="text-white heading">
-                  ✨ What’s the best model for summarizing legal documents?
+          <div className="col-lg-7">
+            <div className="management-card h-100">
+              <div className="custom-card-body">
+                <p className=" mb-2 heading-para">Intelligent time management</p>
+                <p className="small mb-4 description-para">
+                  Manage your AI usage like a pro. Visualize time spent across agents, endpoints, and models—so you can optimize latency, throughput, and productivity.
                 </p>
-                <p className=" description">
-                  The Ai Explain . What’s the best model for summarizing legal
-                  documents.{" "}
-                </p>
-
-                <div className="d-flex gap-2 mt-3 btns-wrapper ">
-                  <button className="btn btn-outline-light btn-sm">
-                    Add details
-                  </button>
-                  <button className="btn btn-outline-light btn-sm">
-                    Suggest something else
-                  </button>
+                <div className="first-icon-circle icon-circle mb-2">
+                  <div className="inner-icon-div">
+                    <div className="border-div">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" focusable="false" color="rgb(181, 127, 247)" style={{ userSelect: 'none', width: '100%', height: '100%', display: 'inline-block', fill: 'rgb(181, 127, 247)', color: 'rgb(181, 127, 247)', flexShrink: 0 }}><g color="rgb(181, 127, 247)" weight="regular"><path d="M200,75.64V40a16,16,0,0,0-16-16H72A16,16,0,0,0,56,40V76a16.07,16.07,0,0,0,6.4,12.8L114.67,128,62.4,167.2A16.07,16.07,0,0,0,56,180v36a16,16,0,0,0,16,16H184a16,16,0,0,0,16-16V180.36a16.09,16.09,0,0,0-6.35-12.77L141.27,128l52.38-39.6A16.05,16.05,0,0,0,200,75.64ZM184,216H72V180l56-42,56,42.35Zm0-140.36L128,118,72,76V40H184Z"></path></g></svg>
+                    </div>
+                  </div>
+                  <div class="icon-bottom-line" ></div>
                 </div>
-                <hr className="my-4" />
-                <p className="mt-3 ask-question small ">
-                  Ask another question ➝
+
+                <div className="chat-box ">
+                  <div className="chat-box-bg">
+                    <div className="bottom-gradient"></div>
+                    <div className="bottom-dot-bg">
+                      <svg width="100%" height="100%" class="text-muted-strong-background inset-0"><defs><pattern id="«R1bacj3kmdb»" width="22" height="22" patternUnits="userSpaceOnUse"><rect x="5.5" y="5.5" width="1" height="1" fill="currentColor"></rect></pattern></defs><rect width="100%" height="100%" fill="url(#«R1bacj3kmdb»)"></rect></svg>
+                    </div>
+                  </div>
+                  <div className="bg-hover-shadow"></div>
+                  <div className="inner-chat-box">
+                    <div className="d-flex flex-column gap-0 h-100">
+                      <div className="d-flex flex-column p-3 flex-grow-1"></div>
+                      <div className="p-3  border-light-2"></div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-5">
+            <div className="integration-card h-100">
+              <div className="custom-card-body">
+                <p className=" mb-2 heading-para">Seamless integrations</p>
+                <p className="small mb-4 description-para">
+                  Connect your AI apps with powerful tools and external APIs effortlessly.
+                  Integrate logging, analytics, vector databases
                 </p>
+                <div className="first-icon-circle icon-circle mb-2">
+                  <div className="inner-icon-div">
+                    <div className="border-div">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" focusable="false" color="rgb(84, 158, 255)" style={{ userSelect: 'none', width: '100%', height: '100%', display: 'inline-block', fill: 'rgb(84, 158, 255)', color: 'rgb(84, 158, 255)', flexShrink: 0 }}><g color="rgb(84, 158, 255)" weight="regular"><path d="M230.91,172A8,8,0,0,1,228,182.91l-96,56a8,8,0,0,1-8.06,0l-96-56A8,8,0,0,1,36,169.09l92,53.65,92-53.65A8,8,0,0,1,230.91,172ZM220,121.09l-92,53.65L36,121.09A8,8,0,0,0,28,134.91l96,56a8,8,0,0,0,8.06,0l96-56A8,8,0,1,0,220,121.09ZM24,80a8,8,0,0,1,4-6.91l96-56a8,8,0,0,1,8.06,0l96,56a8,8,0,0,1,0,13.82l-96,56a8,8,0,0,1-8.06,0l-96-56A8,8,0,0,1,24,80Zm23.88,0L128,126.74,208.12,80,128,33.26Z"></path></g></svg>
+                    </div>
+                  </div>
+                  <div class="icon-bottom-line" ></div>
+                </div>
+
+                <div className="chat-box ">
+                  <div className="chat-box-bg">
+                    <div className="bottom-gradient"></div>
+                    <div className="bottom-dot-bg">
+                      <svg width="100%" height="100%" class="text-muted-strong-background inset-0"><defs><pattern id="«R1bacj3kmdb»" width="22" height="22" patternUnits="userSpaceOnUse"><rect x="5.5" y="5.5" width="1" height="1" fill="currentColor"></rect></pattern></defs><rect width="100%" height="100%" fill="url(#«R1bacj3kmdb»)"></rect></svg>
+                    </div>
+                  </div>
+                  <div className="bg-hover-shadow"></div>
+                  <div className="inner-chat-box">
+                    <div className="d-flex flex-column gap-0 h-100">
+                      <div className="d-flex flex-column p-3 flex-grow-1"></div>
+                      <div className="p-3  border-light-2"></div>
+                    </div>
+                  </div>
+                </div>
+
               </div>
             </div>
           </div>
-          <div className="col-md-5">
-            <div className="custom-card slider-holder h-100">
-              <p className=" mb-2 heading-para">Seamless integrations</p>
-              <p className="small mb-4 description-para">
-                Supercharge AI applications with global large language models,
-                RAG pipelines, tools, agent strategies, and more.
-              </p>
-              <div className=" fourth-icon-circle icon-circle mb-3">
-                <i className="bi bi-lightning-charge-fill fourt-icon"></i>
-              </div>
-              {/* <AiAgentBoxSlider /> */}
-              <div className=" badge">
-                <p className="m-0">connected</p>
-              </div>
-              <div className="mt-5">
-                <AiAgentBoxSlider />
-              </div>
-            </div>
-          </div>
+
         </div>
       </div>
     </section>
